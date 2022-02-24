b22e536426263d9c9f32cad5c806ba154ba69dd7
ProjectPixel16 <jasonflor.cenpri@gmail.com>
2022-02-24 17:04:21 +0800

    Accounting UI

--- application/views/masterfile/login.php
+++ application/views/masterfile/login.php
@@ -1,50 +1,74 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <title>POS - PROGEN</title>
-        <link rel="stylesheet" href="<?php echo base_url(); ?>assets/vendors/mdi/css/materialdesignicons.min.css">
-        <link rel="stylesheet" href="<?php echo base_url(); ?>assets/vendors/css/vendor.bundle.base.css">
+        <meta charset="UTF-8">
+        <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+        <title>Otika - Admin Dashboard Template</title>
+        <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/app.min.css">
+        <link rel="stylesheet" href="<?php echo base_url(); ?>assets/bundles/bootstrap-social/bootstrap-social.css">
         <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/style.css">
-    <link rel="shortcut icon" href="<?php echo base_url(); ?>assets/images/favicon.ico" />
+        <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/components.css">
+        <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/custom.css">
+        <link rel='shortcut icon' type='image/x-icon' href='<?php echo base_url(); ?>assets/img/favicon.ico' />
     </head>
     <body>
-        <div class="container-scroller">
-            <div class="container-fluid page-body-wrapper full-page-wrapper">
-                <div class="content-wrapper d-flex align-items-center auth">
-                    <div class="row flex-grow">
-                        <div class="col-lg-4 mx-auto">
-                            <div class="auth-form-light text-left p-5">
-                                <div class="brand-logo">
-                                    <img src="<?php echo base_url(); ?>assets/images/logo.svg">
+        <div class="loader"></div>
+        <div id="app">
+            <section class="section">
+                <div class="container mt-5">
+                    <div class="row">
+                        <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
+                            <div class="card card-primary">
+                                <div class="card-header">
+                                    <h4>Login</h4>
                                 </div>
-                                <h4>Hello! let's get started</h4>
-                                <h6 class="font-weight-light">Sign in to continue.</h6>
-                                <form class="pt-3">
-                                    <div class="form-group">
-                                        <input type="email" class="form-control form-control-lg" id="exampleInputEmail1" placeholder="Username">
-                                    </div>
-                                    <div class="form-group">
-                                        <input type="password" class="form-control form-control-lg" id="exampleInputPassword1" placeholder="Password">
-                                    </div>
-                                    <div class="mt-3">
-                                        <a class="btn btn-block btn-gradient-info btn-lg font-weight-medium auth-form-btn" href="<?php echo base_url(); ?>index.html">SIGN IN</a>
-                                    </div>
-                                    
-                                    <div class="text-center mt-4 font-weight-light"> 
-                                        Show Dashboard? <a href="<?php echo base_url(); ?>masterfile/dashboard" class="text-primary">Proceed</a>
-                                    </div>  
-                                </form>
+                                <div class="card-body">
+                                    <form method="POST" action="#" class="needs-validation" novalidate="">
+                                        <div class="form-group">
+                                            <label for="email">Email</label>
+                                            <input id="email" type="email" class="form-control" name="email" tabindex="1" required autofocus>
+                                            <div class="invalid-feedback">
+                                            Please fill in your email
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <div class="d-block">
+                                                <label for="password" class="control-label">Password</label>
+                                                <div class="float-right">
+                                                    <a href="auth-forgot-password.html" class="text-small">
+                                                        Forgot Password?
+                                                    </a>
+                                                </div>
+                                            </div>
+                                            <input id="password" type="password" class="form-control" name="password" tabindex="2" required>
+                                            <div class="invalid-feedback">
+                                              please fill in your password
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <div class="custom-control custom-checkbox">
+                                                <input type="checkbox" name="remember" class="custom-control-input" tabindex="3" id="remember-me">
+                                                <label class="custom-control-label" for="remember-me">Remember Me</label>
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <button type="submit" class="btn btn-primary btn-lg btn-block" tabindex="4">
+                                              Login
+                                            </button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                            <div class="mt-5 text-muted text-center">
+                                Don't have an account? <a href="<?php echo base_url(); ?>masterfile/dashboard">Proceed Dashboard</a>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </div>        
+            </section>
+        </div>
+        <script src="<?php echo base_url(); ?>assets/js/app.min.js"></script>
+        <script src="<?php echo base_url(); ?>assets/js/scripts.js"></script>
+        <script src="<?php echo base_url(); ?>assets/js/custom.js"></script>
     </body>
-    <script src="<?php echo base_url(); ?>assets/vendors/js/vendor.bundle.base.js"></script>
-    <script src="<?php echo base_url(); ?>assets/js/off-canvas.js"></script>
-    <script src="<?php echo base_url(); ?>assets/js/hoverable-collapse.js"></script>
-    <script src="<?php echo base_url(); ?>assets/js/misc.js"></script>
 </html>
