bab78b25db375ce73e66e4babbaaf7f5a6470dfe
stephinesev <stephineseverino.cenpri@gmail.com>
2022-06-06 14:26:58 +0800

    Add functionalities in sales ledger

--- assets/js/report.js
+++ assets/js/report.js
@@ -35,3 +35,31 @@ function filterCreditable(){
     var loc= document.getElementById("baseurl").value;
     window.location=loc+'reports/cwht_summary/'+ref+'/'+part;          
 }
+
+function filterLedger(){
+    var ref_no= document.getElementById("ref_no").value;
+    var date_from= document.getElementById("date_from").value;
+    var date_to= document.getElementById("date_to").value;
+    var loc= document.getElementById("baseurl").value;
+
+    if(ref_no!=''){
+        var ref=ref_no;
+    }else{
+        var ref='null';
+    }
+
+
+    if(date_from){
+        var from=date_from;
+    }else{
+        var from='null';
+    }
+
+
+    if(date_to){
+        var to=date_to;
+    }else{
+        var to='null';
+    }
+    window.location=loc+'reports/sales_ledger/'+ref+'/'+from+'/'+to;          
+}
