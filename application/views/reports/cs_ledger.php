f5686d3954e86232808ebd91beca6bf108bb62f5
unknown <glennpaul.cenpri@gmail.com>
2022-06-28 16:37:51 +0800

    design

--- application/views/reports/cs_ledger.php
+++ application/views/reports/cs_ledger.php
@@ -51,9 +51,9 @@
                                 <tr>
                                     <td width="7%"></td>
                                     <td width="13%"><b>Participant Name:</b></td>
-                                    <td width="32%">: <?php echo $part ?></td>
+                                    <td width="32%"><?php echo $part ?></td>
                                     <td width="12%"><b>Date From - To:</b></td>
-                                    <td width="33%">: <?php echo $date_from ?> - <?php echo $date_to ?></td>
+                                    <td width="33%"><?php echo $date_from ?> - <?php echo $date_to ?></td>
                                     <td width="3%"></td>
                                 </tr>
                             </table>
--- application/views/reports/purchases_ledger.php
+++ application/views/reports/purchases_ledger.php
@@ -51,9 +51,9 @@
                                 <tr>
                                     <td width="7%"></td>
                                     <td width="13%"><b>Transaction No.:</b></td>
-                                    <td width="32%">: <?php echo $ref_no ?></td>
+                                    <td width="32%"><?php echo $ref_no ?></td>
                                     <td width="12%"><b>Date From - To:</b></td>
-                                    <td width="33%">: <?php echo $date_from ?> - <?php echo $date_to ?></td>
+                                    <td width="33%"><?php echo $date_from ?> - <?php echo $date_to ?></td>
                                     <td width="3%"></td>
                                 </tr>
                             </table>
