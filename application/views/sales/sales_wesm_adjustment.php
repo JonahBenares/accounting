c081aac6ce2cfae501915045df3970762425bb19
unknown <glennpaul.cenpri@gmail.com>
2022-07-28 11:03:19 +0800

    rename sales to sales adjustment in sales adjustment displaying

--- application/views/sales/sales_wesm_adjustment.php
+++ application/views/sales/sales_wesm_adjustment.php
@@ -7,7 +7,7 @@
                 <div class="col-12 col-md-12 col-lg-12 col-sm-6">
                     <div class="card">
                             <div class="card-header">
-                                <h4>WESM Transaction - Sales</h4>
+                                <h4>WESM Transaction - Sales Adjustment</h4>
                             </div>
                             <div class="card-body">
                                 <form method="POST">
