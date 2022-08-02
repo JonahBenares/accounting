39321a94f3e704e09833e79cebd7d71a434e6707
unknown <glennpaul.cenpri@gmail.com>
2022-08-02 11:16:12 +0800

    fixed masterfile some sub participant not displaying in dropdown

--- application/controllers/Masterfile.php
+++ application/controllers/Masterfile.php
@@ -127,7 +127,7 @@ class Masterfile extends CI_Controller {
         $data['id']=$id;
         //$data['sub_participant'] = $this->super_model->select_custom_where("participant", "participant_id != '$id'");
         //$data['sub_participant']=$this->super_model->select_all_order_by("participant","participant_name","participant_id = '$id'","ASC");
-       $data['sub_participant'] = $this->super_model->custom_query("SELECT DISTINCT * FROM participant p WHERE p.participant_id!='$id' GROUP BY participant_name ORDER BY p.participant_name ASC");
+       $data['sub_participant'] = $this->super_model->custom_query("SELECT DISTINCT * FROM participant p WHERE p.participant_id!='$id' GROUP BY billing_id ORDER BY p.participant_name ASC");
         $rows = $this->super_model->count_rows("subparticipant");
         if($rows!=0){
             foreach($this->super_model->select_custom_where("subparticipant", "participant_id = '$id'") AS $sub){
