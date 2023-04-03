06e6aa11048a01cbd055b17913fc886c07f82bfc
ProjectPixel16 <jasonflor.cenpri@gmail.com>
2023-04-03 23:21:37 +0800

    print OR individual/bulk

---
+++ application/views/sales/PDF_OR_bulk.php
@@ -0,0 +1,156 @@
+<script src="<?php echo base_url(); ?>assets/js/jquery.js"></script>
+<script>
+    function goBack() {
+        window.close();
+      window.history.back();
+    }
+</script>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+    <title>FEBA SYSTEM</title>
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/pdf_or.css">
+    <link rel='shortcut icon' type='image/x-icon' href='<?php echo base_url(); ?>assets/img/logo.png' />
+</head>
+
+<div class="" id="printbutton">
+    <center>
+        
+        <button class="btn btn-warning " onclick="goBack()">Back</button>
+        <button class="btn btn-success " id="counter_print" onclick="printDiv('printableArea')">Print</button>
+        <button class="btn btn-success " onclick="">Save as PDF</button>
+    </center>
+    <br>
+</div>
+<center>
+<div style="padding-bottom:90px;">
+    <div id="contentPDF" >
+    <page size="Long" id="printableArea" class="canvas_div_pdf" >
+        <img class="img2307" src="<?php echo base_url(); ?>assets/img/OR.jpg" style="width: 100%;">
+        <div class="">
+            <label class="date_1">December 10, 2023</label>
+            <label class="ornumber_1">8951</label>
+            <label class="cusname_1">1590 Energy Corporation 1590 Energy Corporation 1590 Energy Corporation 1590 </label>
+            <label class="address_1">907-908 Ayala Life FGU Ctr. Cebu Business Park Luz Cebu City, Cebu City (Capital) Philippines </label>
+            <label class="tin_1">007-099-179-000 </label>
+            <label class="desc_1">TS-WAD-1b5F'2-00000</label>
+            <label class="defint_1">DEF INTEREST</label>
+            <label class="defint_value_1">0.00</label>
+            <label class="energy_1">ENERGY</label>
+            <label class="energy_value_1">2.23</label>
+            <label class="vat_1">VAT</label>
+            <label class="vat_value_1">2.23</label>
+            <label class="total_sales_1">019.23</label>
+            <label class="net_vat_1">019.23</label>
+            <label class="add_vat_1">019.23</label>
+            <label class="total_1">019.23</label>
+            <label class="less_withholding_1">500.23</label>
+            <label class="total_amount_1">500.23</label>
+            <label class="vatable_1">500.23</label>
+            <label class="vat_exempt_1">500.23</label>
+            <label class="zero_rated_1">500.23</label>
+            <label class="vat_percent_1">500.23</label>
+            <label class="grand_total_1">500.23</label>
+            <label class="signature_1">
+                <img src="<?php echo base_url(); ?>assets/img/signature.png" width="100px">
+            </label>
+
+
+
+        </div>
+        <div class="" style="position:absolute;top: 420px;left:-5px">
+            <label class="date_1">December 10, 2023</label>
+            <label class="ornumber_1">8951</label>
+            <label class="cusname_1">1590 Energy Corporation 1590 Energy Corporation 1590 Energy Corporation 1590 </label>
+            <label class="address_1">907-908 Ayala Life FGU Ctr. Cebu Business Park Luz Cebu City, Cebu City (Capital) Philippines </label>
+            <label class="tin_1">007-099-179-000 </label>
+            <label class="desc_1">TS-WAD-1b5F'2-00000</label>
+            <label class="defint_1">DEF INTEREST</label>
+            <label class="defint_value_1">0.00</label>
+            <label class="energy_1">ENERGY</label>
+            <label class="energy_value_1">2.23</label>
+            <label class="vat_1">VAT</label>
+            <label class="vat_value_1">2.23</label>
+            <label class="total_sales_1">019.23</label>
+            <label class="net_vat_1">019.23</label>
+            <label class="add_vat_1">019.23</label>
+            <label class="total_1">019.23</label>
+            <label class="less_withholding_1">500.23</label>
+            <label class="total_amount_1">500.23</label>
+            <label class="vatable_1">500.23</label>
+            <label class="vat_exempt_1">500.23</label>
+            <label class="zero_rated_1">500.23</label>
+            <label class="vat_percent_1">500.23</label>
+            <label class="grand_total_1">500.23</label>
+            <label class="signature_1">
+                <img src="<?php echo base_url(); ?>assets/img/signature.png" width="100px">
+            </label>
+        </div>
+    </page>
+    </div>
+</div>
+</center>
+</html>
+
+<script src="<?php echo base_url(); ?>assets/js/jquery-1.12.4.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/jspdf.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/html2canvas.js"></script>
+<script type="text/javascript">
+    function getPDF(shortname, refno,billing_month, timestamp){
+
+        var HTML_Width = $(".canvas_div_pdf").width();
+        
+        var HTML_Height = $(".canvas_div_pdf").height();
+
+        /*alert(HTML_Height);*/
+        var top_left_margin = 10;
+        var PDF_Width = HTML_Width+(top_left_margin*2);
+        var PDF_Height = (PDF_Width*1.5)+(top_left_margin*2);
+        var canvas_image_width = HTML_Width;
+        var canvas_image_height = HTML_Height;
+        
+        var totalPDFPages = Math.ceil(HTML_Height/PDF_Height)-1;
+        
+
+        html2canvas($(".canvas_div_pdf")[0],{allowTaint:true, 
+            useCORS: true,
+            logging: false,
+            height: window.outerHeight + window.innerHeight,
+            windowHeight: window.outerHeight + window.innerHeight}).then(function(canvas) {
+            canvas.getContext('2d');
+        /*    
+            console.log(canvas.height+"  "+canvas.width);*/
+            
+            
+            var imgData = canvas.toDataURL("image/jpeg", 1.0);
+            var pdf = new jsPDF('p', 'pt',  [PDF_Width, PDF_Height]);
+            pdf.addImage(imgData, 'JPG', top_left_margin, top_left_margin,canvas_image_width,canvas_image_height);
+            
+            
+            for (var i = 1; i <= totalPDFPages; i++) { 
+                pdf.addPage(PDF_Width, PDF_Height);
+                pdf.addImage(imgData, 'JPG', top_left_margin, -(PDF_Height*i)+(top_left_margin*4),canvas_image_width,canvas_image_height);
+            }
+            
+
+            pdf.save("BIR2307_CENPRI_"+shortname+"_"+refno+"_"+billing_month+"_"+timestamp+".pdf");
+        });
+    };
+</script>
+<!-- <script src="<?php echo base_url(); ?>assets/js/jspdf.umd.min.js"></script> -->
+<script type="text/javascript">
+    function printDiv(divName) {
+        var printContents = document.getElementById(divName).innerHTML;
+        var originalContents = document.body.innerHTML;
+
+        document.body.innerHTML = printContents;
+
+        window.print();
+
+        document.body.innerHTML = originalContents;
+    }
+
+   
+</script>
