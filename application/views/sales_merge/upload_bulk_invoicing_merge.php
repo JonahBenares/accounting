ff27f333f410d1899f9e454d9ef6833d32e0c391
Ge3PauL <glennpaul.cenpri@gmail.com>
2025-04-16 14:33:40 +0800

    bulk invoicing main and merge function

--- application/views/sales_merge/upload_bulk_invoicing_merge.php
+++ application/views/sales_merge/upload_bulk_invoicing_merge.php
@@ -1,4 +1,4 @@
-<script src="<?php echo base_url(); ?>assets/js/sales.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/salesmerge.js"></script>
 <div class="main-content">
     <section class="section">
         <div class="section-body">
@@ -6,7 +6,7 @@
                 <div class="col-12 col-md-12 col-lg-12 col-sm-12">
                     <div class="card">
                         <div class="card-header">
-                            <h4>Bulk Upload - Invoicing of Sales (Main)</h4>
+                            <h4>Bulk Upload - Invoicing of Sales (Merge)</h4>
                         </div>
                         <div class="card-body">
                             <form id='bulkinvoicing'> 
@@ -67,10 +67,10 @@
                                             <label><br></label>
                                             <input type='hidden' name='baseurl' id='baseurl' value='<?php echo base_url(); ?>'>
                                             <?php if(empty($year_disp) && empty($reference_number) && empty($due_date) && empty($identifier)){ ?>
-                                                <input type='button' class="btn btn-primary" id='save_bulk_invoicing' type="button" onclick="proceed_sales_main_invoicing()" value="Proceed" style="width:100%">
-                                                 <input type='button' class="btn btn-danger" id="cancel_bulk_invoicing" onclick="cancelSalesMainInvoicing()" value="Cancel Transaction" style='display: none;width:100%'>
+                                                <input type='button' class="btn btn-primary" id='save_bulk_invoicing' type="button" onclick="proceed_sales_merge_invoicing()" value="Proceed" style="width:100%">
+                                                 <input type='button' class="btn btn-danger" id="cancel_bulk_invoicing" onclick="cancelSalesMergeInvoicing()" value="Cancel Transaction" style='display: none;width:100%'>
                                              <?php } elseif ($saved==0){ ?>
-                                                 <input type='button' class="btn btn-danger" id="cancel_bulk_invoicing" onclick="cancelSalesMainInvoicing()" value="Cancel Transaction" style="width:100%">
+                                                 <input type='button' class="btn btn-danger" id="cancel_bulk_invoicing" onclick="cancelSalesMergeInvoicing()" value="Cancel Transaction" style="width:100%">
                                             <?php } ?>
                                         </div>
                                     </div>
@@ -80,17 +80,17 @@
                                 </div> 
                             </form>
                             <?php if($saved==0){ ?>
-                            <form method="POST" id="upload_bulk_invoicing_main">
-                                <div id="upload_bulk_invoicing_main">
+                            <form method="POST" id="upload_bulk_invoicing_merge">
+                                <div id="upload_bulk_invoicing_merge">
                                     <hr>
                                     <?php if(!empty($year_disp) || !empty($reference_number) || !empty($due_date)){ if($saved==0 && $identifier==''){ ?>
-                                    <div class="row">
+                                     <div class="row">
                                         <div class="col-lg-6 col-md-6 offset-md-3 offset-lg-3">
                                             <div class="form-group mb-0">
                                                 <div class="input-group mb-0">
-                                                    <input type="file" class="form-control" name="doc" placeholder="" id="bulkinvoicing_main">
+                                                    <input type="file" class="form-control" name="doc" placeholder="" id="bulkinvoicing_merge">
                                                     <div class="input-group-append">
-                                                        <button class="btn btn-primary" id="proceed_bulkinvoicing_main" onclick="upload_bulkinvoicing_main()"  type="button">Upload</button>
+                                                        <button class="btn btn-primary" id="proceed_bulkinvoicing_merge" onclick="upload_bulkinvoicing_merge()"  type="button">Upload</button>
                                                     </div>
                                                 </div>
                                             </div>
@@ -114,6 +114,7 @@
                                 <table class="table-bordered table table-hover" id="table-7" style="width:100%;">
                                     <thead>
                                         <tr>
+                                            <th width="10%">Reference Number</th>
                                             <th width="10%">Billing ID</th>
                                             <th width="10%">Actual Billing ID</th>
                                             <th width="10%">Invoice No</th>
@@ -124,11 +125,12 @@
                                             foreach($details AS $d){ 
                                                 if(!empty($d['sales_id'])){ 
                                         ?>
-                                    	<tr>
+                                        <tr>
+                                             <td><?php echo $d['reference_no']; ?></td>
                                              <td><?php echo $d['billing_id']; ?></td>
                                             <td><?php echo $d['actual_billing_id']; ?>
                                             <td><?php echo $d['serial_no']; ?></td>
-                                    	</tr>
+                                        </tr>
                                         <?php } } ?>
                                     </tbody>
                                 </table>
@@ -136,7 +138,7 @@
                         </div>
                          <?php if(!empty($details)){ if($saved==0){ ?>
                         <div id='alt' style="font-weight:bold"></div>
-                        <input type="button" id="submitbulkmain" class="btn btn-success btn-md btn-block" onclick="saveBulkInvoicingMain();" value="Save">
+                        <input type="button" id="submitbulkmerge" class="btn btn-success btn-md btn-block" onclick="saveBulkInvoicingMerge();" value="Save">
                         <?php } } } ?>
                     </div>
                 </div>
