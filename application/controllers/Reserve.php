051315270465c992494c92c6e2c77ae080387580
hennetanan <hentanan.cenpri@gmail.com>
2025-10-15 10:23:40 +0800

    consider delete function

--- application/controllers/Reserve.php
+++ application/controllers/Reserve.php
@@ -697,7 +697,7 @@ class Reserve extends CI_Controller {
             $sql.= "pd.scanned_copy = '$scanned_copy' AND ";
         }
         $query=substr($sql,0,-4);
-        $qu = " WHERE adjustment='0' AND saved='1' AND ".$query;
+        $qu = " WHERE adjustment='0' AND saved='1' AND deleted = '0' AND ".$query;
         foreach($this->super_model->custom_query("SELECT * FROM reserve_transaction_details pd INNER JOIN reserve_transaction_head ph ON pd.reserve_id=ph.reserve_id $qu") AS $d){
             $data['or_no'] = $this->super_model->custom_query("SELECT DISTINCT ptd.or_no FROM reserve_transaction_head pth INNER JOIN reserve_transaction_details ptd  WHERE pth.reference_number='$d->reference_number' AND ptd.reserve_id='$d->reserve_id' AND saved='1' AND adjustment='0' ORDER BY or_no ASC");
             $create_date = $this->super_model->select_column_where("reserve_transaction_head", "create_date", "reserve_id", $d->reserve_id);
